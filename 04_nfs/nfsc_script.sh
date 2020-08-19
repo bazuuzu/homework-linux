865dfbcf46f334bf62867b1575a5c721b556e12a
Sergey Novoselov <sna1030@gmail.com>
2020-08-19 19:38:00 +0300

    hw 4

--- 04_nfs/nfsc_script.sh
+++ 04_nfs/nfsc_script.sh
@@ -7,7 +7,14 @@ systemctl start rpcbind
 systemctl enable rpcbind
 # Создаём папку в монтировании
 mkdir /mnt/upload
+# ---- Блок работает чере раз после перезагрузки ---- #
 # Прописываем в автомонтирование папку
-echo "192.168.50.10:/mnt/upload /mnt/upload nfs udp,nfsvers=3 0 0" >> /etc/fstab
+# echo "192.168.50.10:/mnt/upload /mnt/upload nfs noauto,x-systemd.automount,udp,nfsvers=3 0 0" >> /etc/fstab
 # Примонтируем всё из fstab
-mount -a
+# mount -a
+# ---- Конец блока ---- #
+# Смотируем через systemd. Имя файла должно содержать имя точки монтирования
+echo -e "[Unit]\n  Description=nfs mount script\n  Requires=network-online.target\n  After=network-online.service\n\n[Mount]\n  What=192.168.50.10:/mnt/upload\n  Where=/mnt/upload\n  Options=udp,nfsvers=3\n  Type=nfs\n\n[Install]\n  WantedBy=multi-user.target" > /etc/systemd/system/mnt-upload.mount
+
+systemctl enable mnt-upload.mount
+systemctl start mnt-upload.mount
