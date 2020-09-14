1f9ac03f4c88fcd8bed7e1852c1d8884fefba889
Sergey Novoselov <sna1030@gmail.com>
2020-09-14 15:07:56 +0300

    hw 7

---
+++ 07_bash/req.sh
@@ -0,0 +1,63 @@
+#!/bin/bash
+
+#ENV
+old_str=`cat env.txt`
+lockfile=/tmp/localfile
+
+#Функция подсчёта временного промежутка
+timeinterval()
+    {
+        echo "Временной интервал:" > send.txt
+        diap1=$(tail -n+$old_str access.log | awk -F " " '{print $4}' | cut -c 2- | awk 'NR==1')
+        diap2=$(tail -n+$old_str access.log | awk -F " " '{print $4}' | cut -c 2- | awk 'END{print}')
+        echo "$diap1 - $diap2" >> send.txt
+    }
+
+#Функция подсчёта значений
+
+parsing()
+{
+        #X IP адресов (с наибольшим кол-вом запросов) с указанием кол-ва запросов
+        echo "IP адресов с наибольшим кол-вом запросов:" >> send.txt
+        tail -n+$old_str access.log | awk -F "-" '{print $1}' | sort -nr | uniq -c | sort -nr >> send.t$
+        #Y запрашиваемых адресов (с наибольшим кол-вом запросов) с указанием кол-ва запросов c момента $
+        echo "Наиболее запрашиваемые адреса:" >> send.txt
+        tail -n+$old_str access.log | awk -F " " '{print $7}' | sort -nr | uniq -c | sort -nr >> send.t$
+        #все ошибки c момента последнего запуска
+        echo "Все ошибки:" >> send.txt
+        tail -n+$old_str access.log | awk -F '"' '{print $3}' | awk -F " " '{print $1}' | grep -P '4..|$
+        #список всех кодов возврата с указанием их кол-ва с момента последнего запуска
+        echo "Все коды возврата:" >> send.txt
+        tail -n+$old_str access.log | awk -F '"' '{print $3}' | awk -F " " '{print $1}' | sort | uniq -$
+}
+
+#Функция изменения строки
+str_chn()
+{
+old_str=`wc -l access.log | awk -F " " '{print $1}'`
+echo ${old_str} > env.txt
+}
+
+trap_file()
+{
+        if ( set -o noclobber; echo `ps -a | grep req.sh` > "$lockfile") 2> /dev/null;
+                then
+                trap 'rm -f "$lockfile"; exit $?' INT TERM EXIT
+                while true
+                        do
+                        timeinterval
+                        parsing
+                        str_chn
+                        sleep 10s
+                        mail -s "Stat access.log" root@localhost < send.txt
+                        exit
+                done
+                rm -rf "$lockfile"
+                trap - INT TERM EXIT
+        else
+                echo "Failed to launch script"
+                echo "Held by $(cat $lockfile)"
+        fi
+}
+
+trap_file
